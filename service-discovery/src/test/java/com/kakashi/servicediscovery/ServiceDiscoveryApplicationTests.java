6bf5f92fa97a477aab61c3ea34557fd57ef5ed4d
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-09-26 17:08:52 +0530

    initial commit for service-discovery

---
+++ service-discovery/src/test/java/com/kakashi/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakashi.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
